c94469e18ac785268eee5d0f0ad6861e88af47df
Markus <markus@ubuntu.(none)>
2012-07-26 13:42:06 +0200

    initial setup with mysql db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transfair::Application.config.secret_token = '7ac1d4d84afc8b9a5919d2c22255906b80b0f65a27c51c31500271ce3a335353ae4ee6109f5b2b459b5cbb7d62deb6db4184ed5088890059948bec5decf9d9f3'
