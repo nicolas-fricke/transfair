d3dcbbc4a4d773193e748e6b1f6b40299a892cb5
markus-hinsche <m.hinsche@gmail.com>
2012-07-29 16:30:02 +0200

    changes to navigation, simple search for client index view

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,7 +2,7 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.all
+    @clients = Client.search(params[:search])
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,15 @@
 class Client < ActiveRecord::Base
   attr_accessible :company, :email, :name, :phone_number, :position, :surname
+
+
+
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
+
+
 end
