1f1ce4b1858dbff80fb129d4a8991470a122bfc0
markus-hinsche <m.hinsche@gmail.com>
2012-08-01 11:22:22 +0200

    views show content we want now

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,6 @@ class Job < ActiveRecord::Base
   belongs_to :client
   belongs_to :job_type
   belongs_to :job_status
-  attr_accessible :client_paid, :deadline_client, :deadline_intern, :name, :rating, :rating_text, :client,
-                  :job_type, :job_status
+  attr_accessible :client_paid, :deadline_client, :deadline_intern, :name, :rating, :rating_text,
+                  :client_id, :client, :job_type_id, :job_status_id
 end
--- app/models/service_partner.rb
+++ app/models/service_partner.rb
@@ -1,6 +1,12 @@
 class ServicePartner < ActiveRecord::Base
   belongs_to :country
   belongs_to :education
-  attr_accessible :birth_year, :city, :education_background, :email, :internet_access, :internet_use, :is_urban, :name,
-                  :phone_number, :surname, :work_experience, :country, :education
+  attr_accessible :birth_year, :city, :education_background, :email, :internet_access,
+                  :internet_use, :is_urban, :name,
+                  :phone_number, :surname, :work_experience, :country_id, :education_id
+
+  def full_name
+    "#{self.name} #{self.surname}"
+  end
+
 end
