aea0c00d5236cf7ec5d2ddc415c3a2619c9044b1
markus-hinsche <m.hinsche@gmail.com>
2012-08-01 12:07:04 +0200

    improve view with dropdown boxes, refactorings

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,17 @@ class Job < ActiveRecord::Base
   belongs_to :job_status
   attr_accessible :client_paid, :deadline_client, :deadline_intern, :name, :rating, :rating_text,
                   :client_id, :client, :job_type_id, :job_status_id
+
+  def client
+    Client.find_by_id(self.client_id)
+  end
+
+  def job_type
+    JobType.find_by_id(self.job_type_id)
+  end
+
+  def job_status
+    JobStatus.find_by_id(self.job_status_id)
+  end
+
 end
