27337f6653d586ca64b9677d960dd33ab7147f2b
markus-hinsche <m.hinsche@gmail.com>
2012-08-05 23:38:51 +0200

    job accessors killed, works without, dunno why

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,21 +3,9 @@ class Job < ActiveRecord::Base
   belongs_to :job_type
   belongs_to :job_status
   attr_accessible :client_paid, :name, :rating_client, :rating_text,
-                  :client_id, :client, :job_type_id, :job_status_id
+                  :job_type_id, :job_status_id
 
   has_many :employments
   has_many :service_partners, :through => :employments
-
-  def client
-    Client.find_by_id(self.client_id)
-  end
-
-  def job_type
-    JobType.find_by_id(self.job_type_id)
-  end
-
-  def job_status
-    JobStatus.find_by_id(self.job_status_id)
-  end
 
 end
