a828ea6333d090720dc3ea5fe77551d57fbb0d64
markus-hinsche <m.hinsche@gmail.com>
2012-08-05 17:54:49 +0200

    migrations date_of_birth of sp

--- app/models/service_partner.rb
+++ app/models/service_partner.rb
@@ -1,7 +1,7 @@
 class ServicePartner < ActiveRecord::Base
   belongs_to :country
   belongs_to :education
-  attr_accessible :birth_year, :city, :education_background, :email, :internet_access,
+  attr_accessible :date_of_birth, :city, :education_background, :email, :internet_access,
                   :internet_use, :is_urban, :name,
                   :phone_number, :surname, :work_experience, :country_id, :education_id
 
