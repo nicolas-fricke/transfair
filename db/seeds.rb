63c096993009abf91a284873e5505312ebc32e48
François Briod <f.briod@gmail.com>
2012-08-07 21:14:22 +0300

    Update db/seeds.rb
    Add some seeds data for every table unless the jobs (as well as employment and user) table because I am unsure on how to make the connection with SPs and clients...

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,11 +48,29 @@ Country.create([
 
 Client.delete_all
 Client.create([
-  {:company => 'Amnesty International', :email => 'info@amnesty.org', :name => 'Charlize',
-   :phone_number => '123-1221235', :position => 'accountant', :surname => 'Foster',
+  {:company => 'Steinbeis University', :email => 'bbulatovic@steinbeis-icrm.eu', :name => 'Boris',
+   :phone_number => '', :position => 'coordinator course of study "responsible management"', :surname => 'Bulatovic',
    :country_id =>Country.first.id, :affiliation => Affiliation.first},
-  {:company => 'Welthungerhilfe', :email => 'mayer@welthunger.co', :name => 'Jochen',
-   :phone_number => '+49/0440/12341', :position => 'CEO', :surname => 'Mayer',
+  {:company => 'HSFK', :email => 'fey@hsfk.de', :name => 'Marco',
+   :phone_number => '', :position => 'PhD Student', :surname => 'Fey',
+   :country_id =>Country.first.id, :affiliation => Affiliation.first},
+  {:company => 'HSFK', :email => 'poppe@hsfk.de', :name => 'Annika',
+   :phone_number => '', :position => 'PhD Student', :surname => 'Poppe',
+   :country_id =>Country.first.id, :affiliation => Affiliation.first},
+  {:company => 'iq-consult', :email => 'chri_noel@hotmail.com', :name => 'Christing',
+   :phone_number => '', :position => 'intern', :surname => 'Noel',
+   :country_id =>Country.first.id, :affiliation => Affiliation.first},
+  {:company => 'HSFK', :email => 'herr@hsfk.de', :name => 'Steffi',
+   :phone_number => '', :position => 'PhD Student', :surname => 'Herr',
+   :country_id =>Country.first.id, :affiliation => Affiliation.first},
+  {:company => 'project winch 5', :email => 'pisani.winch5@gmail.com', :name => 'Francis',
+   :phone_number => '', :position => 'journalist', :surname => 'Pisani',
+   :country_id =>Country.first.id, :affiliation => Affiliation.first},
+  {:company => 'Robotspodcast', :email => 'adrien.briod@robotspodcast.com', :name => 'Adrien',
+   :phone_number => '+41788317234', :position => '', :surname => 'Briod',
+   :country_id =>Country.first.id, :affiliation => Affiliation.first},
+  {:company => 'Löwenherz Grafikdesign', :email => 'jan@janloewenherz.com', :name => 'Jan',
+   :phone_number => '', :position => 'freelancer', :surname => 'Löwenherz',
    :country_id =>Country.first.id, :affiliation => Affiliation.first},
 ])
 
@@ -61,8 +79,7 @@ Education.create([
   {:name => 'primary education'},
   {:name => 'middle school'},
   {:name => 'high school'},
-  {:name => 'tertiary education'},
-  {:name => 'university'},
+  {:name => 'tertiary education/university'},
   {:name => 'other'},
 ])
 
@@ -71,8 +88,27 @@ Education.create([
 
 FairWage.delete_all
 FairWage.create([
-  {:country_id => Country.first.id, :amount => 10, :date => Date.today},
-  {:country_id => Country.first.id, :amount => 12, :date => Date.today - 31.days},
+  {:country_id => Country.find_by_name('Bangladesh').id, :amount => 125, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Bolivia').id, :amount => 12.5, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Cameroon').id, :amount => 825, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('China').id, :amount => 11, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Ghana').id, :amount => 2.75, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('India').id, :amount => 85, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Indonesia').id, :amount => 15375, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Kazakhstan').id, :amount => 250, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Kenya').id, :amount => 175, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Malawi').id, :amount => 275, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Nigeria').id, :amount => 275, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Pakistan').id, :amount => 150, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Papa New Guinea').id, :amount => 3.75, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Philippines').id, :amount => 75, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Sri Lanka').id, :amount => 187.5, :date => Date.new(2012,8,7),
+  {:country_id => Country.find_by_name('Tanzania').id, :amount => 2875, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Thailand').id, :amount => 52.5, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Uganda').id, :amount => 5000, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Vietnam').id, :amount => 36250, :date => Date.new(2012,8,7)},
+  {:country_id => Country.find_by_name('Zimbabwe').id, :amount => 1.75, :date => Date.new(2012,8,7)},
+
 ])
 
 
@@ -81,15 +117,21 @@ JobStatus.create([
   {:name => 'intention'},
   {:name => 'paused'},
   {:name => 'defined'},
-  {:name => 'waiting for available service partner'},
-  {:name => 'waiting for confirmation'},
-  {:name => 'cannot do it in time'},
-  {:name => 'no feedback'},
+  {:name => 'waiting'},
+  {:name => 'waiting for deadline extension'},
   {:name => 'offered'},
+  {:name => 'cancelled'},
+  {:name => 'accepted'},
   {:name => 'handed in'},
-  {:name => 'proposition to spot-check'},
+  {:name => 'late'},
+  {:name => 'late without news'},
+  {:name => 'offered to spot-check'},
   {:name => 'sent to spot-check'},
-  {:name => 'spot-checked'},
+  {:name => 'spot-check by TF'},
+  {:name => 'cannot do it in time'},
+  {:name => 'recieved revised version'},
+  {:name => 'setn back to SP'},
+  {:name => 'delivered to customer'},
   {:name => 'to be rated'},
   {:name => 'rated'},
   {:name => 'completed'},
@@ -99,16 +141,14 @@ JobType.delete_all
 JobType.create([
    {:name => 'transcription'},
    {:name => 'translation'},
-   {:name => 'online research'},
+   {:name => 'research assistance'},
 ])
 
 Language.delete_all
 Language.create([
   {:name => 'German'},
   {:name => 'English'},
-  {:name => 'Spanish'},
   {:name => 'French'},
-  {:name => 'Italian'},
 ])
 
 Job.delete_all
@@ -126,15 +166,15 @@ Job.create([
 
 ServicePartner.delete_all
 sp1 = ServicePartner.create(
-  :country_id => Country.find_by_name('Malawi').id, :education_id => Education.first.id, :date_of_birth => Date.new(1989,6,1), :city => 'Zomba',
-  :education_background => '', :email => 'malawiguy@gmail.com', :internet_access => 'sometimes',
-  :internet_use => 'often', :is_urban => true, :name => 'malawiguy', :phone_number => '123-123123',
-  :surname => '', :work_experience => 'some')
+  :country_id => Country.find_by_name('Kenya').id, :education_id => Education.find_by_name('tertiary education/university').id :date_of_birth => Date.new(1979,4,27), :city => '',
+  :education_background => 'I have studied economics to a diploma level among other certificates.', :email => 'ngugij12@gmail.com', :internet_access => 'at home',
+  :internet_use => '2-3 times per week', :is_urban => true, :name => 'James', :phone_number => '',
+  :surname => 'Ngugi', :work_experience => 'I have been self employed but finding it hard to sustain because of the economic situation in the country.')
 sp2 = ServicePartner.create(
-  :country_id => Country.find_by_name('Ghana').id, :education_id => Education.find_by_name('middle school').id, :date_of_birth => Date.new(1989,6,1), :city => 'Axim',
-  :education_background => 'have been to Europe for study 1 semester', :email => 'ghsh@blue.com', :internet_access => '',
-  :internet_use => 'often', :is_urban => true, :name => 'rasta', :phone_number => '123-5555123',
-  :surname => 'zasta', :work_experience => 'experience and hard worker')
+  :country_id => Country.find_by_name('Uganda').id, :education_id => Education.find_by_name('tertiary education/university').id, :date_of_birth => Date.new(1989,4,4), :city => '',
+  :education_background => 'I did Literature, Economics, History and Divinity in A Level at Kings College Budo.I have also finished my three years Bachelor of Science Degree in Accounting and Finance at Kyambogo University this year in May.', :email => 'tom.kalyesubula@aiesec.net', :internet_access => 'at home',
+  :internet_use => 'every days', :is_urban => true, :name => 'Tom', :phone_number => '+2556779222883',
+  :surname => 'Kalyesubula', :work_experience => 'I have been working part time for AIESEC Uganda for the past two years. First as Local chapter Vice president for Finance and Administration, then for a year as Local Chapter President and now as the National Vice President for Product and Business Development.I was also while being president at the University worked a day job at Village Energy Company Limited as Finance and Administration Manager.')
 
 ServicePartner.create([
 ])
