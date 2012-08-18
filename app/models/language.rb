1a3d8a9340301613b901748c598f35085304d37f
markus-hinsche <m.hinsche@gmail.com>
2012-08-18 14:03:38 +0200

    add missing relations

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,6 @@
 class Language < ActiveRecord::Base
   attr_accessible :name
   has_many :jobs
+  has_many :qualification_tests
   validates :name, :presence => true
 end
--- app/models/transcription_information.rb
+++ app/models/transcription_information.rb
@@ -2,6 +2,7 @@ class TranscriptionInformation < ActiveRecord::Base
 
   attr_accessible :audio_quality, :background_information, :category_id, :duration, :ending_point, :field_id, :is_content_focused, :is_detailed_transcription, :number_speaker, :recurring_denominations, :speaker_accent, :starting_point, :with_emotion_interjection, :with_interruption, :with_pause, :with_pause_interjection, :with_time_stamp
 
+  belongs_to :category
   has_and_belongs_to_many :origin_of_speakers, :class_name => "Country"
   has_one :job
 
