c7b2e089ee4e536fe1f0f1a650b3161ead27cfaa
Nicolas Fricke <nicolas.fricke@gmail.com>
2012-09-02 19:49:40 +0200

    Created simple_form template for date and time pickers. Shortcoming: a date_time_picker does NOT exist. Needs to be split up into one picker for the date and another for the time.

--- app/inputs/date_time_input.rb
+++ app/inputs/date_time_input.rb
@@ -1,6 +1,14 @@
 class DateTimeInput < SimpleForm::Inputs::DateTimeInput
   def input
-    @attribute_name = attribute_name
-    render :partial => 'date'
+    if input_html_options[:class].include? :time
+      input_html_options[:class] << [:timepicker]
+      input_field = "#{@builder.text_field(attribute_name, input_html_options)}"
+      icon = "<i class='' style='margin: -2px 0 0 -26px; pointer-events: none; position: relative;'></i>"
+    else
+      input_html_options[:class] << [:datepicker]
+      input_field = "#{@builder.text_field(attribute_name, input_html_options)}"
+      icon = "<i class='icon-calendar' style='margin: -2px 0 0 -26px; pointer-events: none; position: relative;'></i>"
+    end
+    "#{input_field} #{icon}".html_safe
   end
 end
