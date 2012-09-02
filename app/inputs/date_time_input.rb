ec99e1201dda319198a259a0a5f04c9ff9488184
Nicolas Fricke <nicolas.fricke@gmail.com>
2012-09-02 19:53:16 +0200

    Added icon to time picker. Also, fixed deadline picker back to a date picker.

--- app/inputs/date_time_input.rb
+++ app/inputs/date_time_input.rb
@@ -3,7 +3,7 @@ class DateTimeInput < SimpleForm::Inputs::DateTimeInput
     if input_html_options[:class].include? :time
       input_html_options[:class] << [:timepicker]
       input_field = "#{@builder.text_field(attribute_name, input_html_options)}"
-      icon = "<i class='' style='margin: -2px 0 0 -26px; pointer-events: none; position: relative;'></i>"
+      icon = "<i class='icon-time' style='margin: -2px 0 0 -26px; pointer-events: none; position: relative;'></i>"
     else
       input_html_options[:class] << [:datepicker]
       input_field = "#{@builder.text_field(attribute_name, input_html_options)}"
